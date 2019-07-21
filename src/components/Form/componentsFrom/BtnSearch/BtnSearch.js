03dd154ed4b1e7a9edcf0fec470236b7327a03bc
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2019-07-21 21:21:12 +0300

    movies/fix: fixed movie search error by input value and rendering film content

--- src/components/Form/componentsFrom/BtnSearch/BtnSearch.js
+++ src/components/Form/componentsFrom/BtnSearch/BtnSearch.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const BtnSearch = props => <button onClick={props.getMovis}>SEARCH</button>
+const BtnSearch = props => {
+    return (
+        <button onClick={props.getMovis}>SEARCH</button>
+    )
+}
 
 export default BtnSearch
--- src/components/Form/componentsFrom/Input/Input.js
+++ src/components/Form/componentsFrom/Input/Input.js
@@ -1,11 +1,44 @@
 import React from 'react'
 
-const Input = props => {
-   //console.log(props)
-    const onChange = e => props.getInpVal(e.target.value);
-    return(
-        <input onChange={onChange}/>
-    )
+class Input extends React.Component {
+    constructor(props){
+        super(props)
+        console.log(props)
+        // this.inputVal.bind(this)
+    }
+
+    inputVal(){
+        if(this.film){
+            console.log(this.film)
+            this.props.getInpVal(this.film.value)
+        } else {
+            return ''
+        }
+    }
+
+    render(){
+        this.inputVal()
+        return(
+            <input ref={(input) => {this.film = input}}/>
+        )
+    }
 }
 
 export default Input
+
+
+
+
+
+
+// const Input = props => {
+//    //console.log(props)
+
+//     const onChange = e => props.getInpVal(e.target.value);
+//     return(
+//         <input onChange={onChange} />
+//     )
+// }
+
+// export default Input
+
